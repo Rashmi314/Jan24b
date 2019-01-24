598ed3e0819f14fd3c8f5fe4079d2240cfb1b4d0
Rashmi314 <46991593+Rashmi314@users.noreply.github.com>
2019-01-24 22:59:57 +0530

    some modify

---
+++ src/qsp/demoC.java
@@ -0,0 +1,12 @@
+package qsp;
+
+public class demoC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		System.out.println(" entered number is ");
+
+	}
+
+}
